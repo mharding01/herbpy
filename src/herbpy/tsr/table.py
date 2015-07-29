b64dbc0a24eeb462395b3b84a80c28bf54ca72d0
Matthew Harding <mharding@andrew.cmu.edu>
2015-07-29 09:42:24 -0400

    tabletop_reaching experiments-related tsrs

--- src/herbpy/tsr/table.py
+++ src/herbpy/tsr/table.py
@@ -38,3 +38,38 @@ def point_on(robot, table, manip=None):
     table_top_chain = TSRChain(sample_start = False, sample_goal = True, constrain=False, 
                                TSR = table_top_tsr)
     return [table_top_chain]
+
+# Why does this not work with 'conference_table' as second argument?
+@TSRFactory('herb', 'table', 'given_point_on')
+def given_point_on(robot, table, given_table_point, manip=None):
+    '''
+    This creates a TSR that allows you to give a particular point to compute TSR to.
+    They are directly on the table, and thus not suitable as an end-effector pose.
+    Grasp specific calculations are necessary to find a suitable end-effector pose.
+
+    @param robot The robot performing the grasp
+    @param pitcher The pitcher to grasp
+    @param manip The manipulator to perform the grasp, if None
+       the active manipulator on the robot is used
+    '''
+    if manip is None:
+        manip_idx = robot.GetActiveManipulatorIndex()
+    else:
+        with manip.GetRobot():
+            manip.SetActive()
+            manip_idx = manip.GetRobot().GetActiveManipulatorIndex()
+           
+    world_point_on_table = numpy.dot(table.GetTransform(), given_table_point);
+    T0_w = world_point_on_table;    # We want to rotate around the point on table itself
+
+    # The frame is set on the table such that the y-axis is normal to the table surface
+    Tw_e = numpy.eye(4); # Give the EE the frame at the point (translated from world to table frame) 
+    
+    Bw = numpy.zeros((6,2))
+    Bw[5,:] = [-numpy.pi, numpy.pi] # allow any rotation around z - which is the (world!) axis normal to the table top
+    
+    table_top_tsr = TSR(T0_w = T0_w, Tw_e = Tw_e, Bw = Bw, manip = manip_idx)
+    table_top_chain = TSRChain(sample_start = False, sample_goal = True, constrain=False, 
+                               TSR = table_top_tsr)
+    return [table_top_chain]
+
